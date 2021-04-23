c41034716448a2a9e0f6f2fea094babfed1b4729
Oscar de Jaime Alvarez <oscar.dejaime@metrica.es>
2021-04-23 20:30:01 +0200

    Initial Commit

---
+++ src/main/java/com/ProyectoASO/AsoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoASO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoASO/AsoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoASO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

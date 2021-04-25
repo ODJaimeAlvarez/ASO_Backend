73fd25c1464d2624ba229d9273d36bee38017ad5
adri <adrianml122@gmail.com>
2021-04-25 19:42:21 +0200

    Adición de runner

---
+++ src/main/java/com/ProyectoASO/configuration/Runner.java
@@ -0,0 +1,40 @@
+package com.ProyectoASO.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ProyectoASO.dao.IUsuarioDao;
+import com.ProyectoASO.entity.Usuario;
+
+@Configuration
+public class Runner {
+
+	@Autowired
+	IUsuarioDao user;
+
+	@Bean
+	public ApplicationRunner meterUser() {
+
+		return arg -> meterUsuarioRunner();
+	}
+
+	private void meterUsuarioRunner() {
+
+		List<Usuario> list_user = new ArrayList<>();
+
+		list_user.add(new Usuario("Martos", "Lopez", "adri@hotmail.com", "uwuwu", true, "Adrian"));
+		list_user.add(new Usuario("De Jaime", "Alvarez", "oscar@hotmail.com", "uwito", true, "Oscar"));
+		list_user.add(new Usuario("Pelayo", "Mellas", "salma@hotmail.com", "awitademadrid", true, "Salma"));
+		list_user.add(new Usuario("Garcia", "Muro", "jose@hotmail.com", "shreckriko", true, "Jose"));
+		list_user.add(new Usuario("Perez", "Gonzales", "david@hotmail.com", "violinistacion", true, "David"));
+
+		user.saveAll(list_user);
+
+	}
+
+}
